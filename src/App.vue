ee421af6f84c68f7362e5f385b23df3a2b2a7ea1
#3 <931606284@qq.com>
2022-11-23 15:55:46 +0800

    feat:easy plum

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,112 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from 'vue';
+
+const el = ref<HTMLCanvasElement>()
+
+const WIDTH = 600
+const HEIGHT = 600
+
+interface Point{
+  x:number,
+  y:number
+}
+
+interface Branch {
+  start:Point,
+  length:number,
+  theta:number
+}
+
+function init(){
+  const canvas = el.value!
+  const ctx = canvas.getContext('2d')!
+  ctx.strokeStyle = "pink"
+
+  const branch:Branch = {
+    start:{ x:WIDTH /2,y:HEIGHT},
+    length:40,
+    theta:-Math.PI / 2
+  }
+  step(branch)
+  // lineTo(startPoint,endPoint)
+  // lineTo(endPoint,{x:WIDTH/2+50,y:10})
+  
+}
+
+
+function step(b:Branch){
+  const end = getBranch(b)
+  drawBranch(b)
+  const endPoint = {
+    x:WIDTH/2,
+    y:HEIGHT/2
+  }
+  if(Math.random() < 0.5){
+    step( {
+      start: end,
+      length: b.length,
+      theta: b.theta - 0.2
+    })
+  }
+  if(Math.random() < 0.5){
+    step({
+      start: end,
+      length: b.length,
+      theta: b.theta + 0.2
+    })
+  }
+
+  // let leftPoint = {
+  //   start: end,
+  //   length: 100,
+  //   theta: b.theta - 0.1
+  // }
+
+  // drawBranch(leftPoint)
+
+  // let rightPoint = {
+  //   start: end,
+  //   length: 100,
+  //   theta: b.theta + 0.1
+  // }
+  // drawBranch(rightPoint)
+}
+
+function lineTo(p1:Point,p2:Point){
+  const canvas = el.value!
+  const ctx = canvas.getContext('2d')!
+  ctx.beginPath()
+  ctx.moveTo(p1.x,p1.y)
+  ctx.lineTo(p2.x,p2.y)
+  ctx.stroke()
+}
+
+function getBranch(b:Branch){
+  const { start,length,theta } = b
+  return {
+     x:start.x +length * Math.cos(theta),
+      y:start.y +length * Math.sin(theta),
+  }
+}
+
+function drawBranch(b:Branch){
+  const { start,length,theta } = b
+  // const end = {
+  //   x:start.x +length * Math.cos(theta),
+  //   y:start.y +length * Math.sin(theta),
+  // }
+  lineTo(start,getBranch(b))
+}
+
+onMounted(() => {
+  init()
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <canvas ref="el" width="500" height="500 " style="border: 1px solid #fff"></canvas>
+    <div>hi</div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/template.vue
@@ -0,0 +1,90 @@
+<template lang='pug'>
+paper
+  .box.overflow-hidden(@click='f.start')
+    canvas(ref='el' width='400' height='400')
+  .box-description.py-2.flex(v-if='!shot')
+    .flex(@click='init = init % 8 + 1')
+      .text-gray-400 init
+      .text-gray-500.bold.px-2 {{init}}
+    .flex.ml-3(@click='len = len % 8 + 1')
+      .text-gray-400 len
+      .text-gray-500.bold.px-2 {{len}}
+    .flex-auto
+    .text-gray-400(v-if='!stopped') *
+
+note
+  .p for each node, there is 50% chance for growing a new branch for its two branches.
+  br
+  .p <b>init</b> - the initial iterations that guaranteed having 100% to grow.
+  .p <b>len</b> - max length for each branch.
+</template>
+
+<script setup='props' lang='ts'>
+import { useRafFn } from '@vueuse/core'
+import { useRouteQuery } from '@vueuse/router'
+import { onMounted, ref, watch } from 'vue'
+import { initCanvas, polar2cart, r15, r180, r90 } from '../utils'
+const shot = useRouteQuery('shot')
+const el = ref<HTMLCanvasElement | null>(null)
+const { random } = Math
+const f = {
+  start: () => {},
+}
+const init = ref(5)
+const len = ref(5)
+const stopped = ref(false)
+watch([init, len], () => f.start())
+onMounted(async() => {
+  const canvas = el.value!
+  const { ctx } = initCanvas(canvas)
+  const { width, height } = canvas
+  let steps: Function[] = []
+  let prevSteps: Function[] = []
+  let iterations = 0
+  const step = (x: number, y: number, rad: number) => {
+    const length = random() * len.value
+    const [nx, ny] = polar2cart(x, y, length, rad)
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+    ctx.lineTo(nx, ny)
+    ctx.stroke()
+    const rad1 = rad + random() * r15
+    const rad2 = rad - random() * r15
+    if (nx < -100 || nx > 500 || ny < -100 || ny > 500)
+      return
+    if (iterations <= init.value || random() > 0.5)
+      steps.push(() => step(nx, ny, rad1))
+    if (iterations <= init.value || random() > 0.5)
+      steps.push(() => step(nx, ny, rad2))
+  }
+  const frame = () => {
+    iterations += 1
+    prevSteps = steps
+    steps = []
+    if (!prevSteps.length) {
+      controls.pause()
+      stopped.value = true
+    }
+    prevSteps.forEach(i => i())
+  }
+  const controls = useRafFn(frame, { immediate: false })
+  f.start = () => {
+    controls.pause()
+    iterations = 0
+    ctx.clearRect(0, 0, width, height)
+    ctx.lineWidth = 1
+    ctx.strokeStyle = '#00000040'
+    prevSteps = []
+    steps = random() < 0.5 ? [
+      () => step(0, random() * 400, 0),
+      () => step(400, random() * 400, r180),
+    ] : [
+      () => step(random() * 400, 0, r90),
+      () => step(random() * 400, 400, -r90),
+    ]
+    controls.resume()
+    stopped.value = false
+  }
+  f.start()
+})
+</script>
